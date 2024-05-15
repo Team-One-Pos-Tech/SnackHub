30a5b58c4ed90f56d62bc0b6257426c35af8a98c
Icaro Sousa <icaro.py@gmail.com>
2024-05-16 00:21:39 +0100

    feat: adds github ci workflows[build, publish] and move projects into folders

--- src/SnackHub.Api/Program.cs
+++ src/SnackHub.Api/Program.cs
@@ -20,16 +20,13 @@ builder.Services.AddScoped<IGetClientUseCase, GetClientUseCase>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
