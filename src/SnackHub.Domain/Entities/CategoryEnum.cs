5598768e0206c4eedc0e904be19c37746e329478
tiago.franck <tiago.franck@marketup.com>
2024-05-17 17:16:59 -0300

    Add Product Entities

---
+++ src/SnackHub.Domain/Entities/CategoryEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackHub.Domain.Entities
+{
+    public enum Category
+    {
+        Lanche,
+        Acompanhamento,
+        Bebida,
+        Sobremesa
+    }
+}
---
+++ src/SnackHub.Domain/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SnackHub.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Category Category { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+        public List<string> Image { get; private set; }
+
+        public Product(string name, Category category, decimal price, string description, List<string> image)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Category = category;
+            Price = price;
+            Description = description;
+            Image = image ?? new List<string>();
+        }
+
+        public void Edit(string name, Category categoria, decimal price, string description, List<string> image)
+        {
+            Name = name;
+            Category = categoria;
+            Price = price;
+            Description = description;
+            Image = image ?? new List<string>();
+        }
+    }
+}
