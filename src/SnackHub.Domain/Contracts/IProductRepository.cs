a1c66bd077c8e5eeddfbae0548593cc8bd42e84d
Tiagofrik <bicalho.franck.tiago@gmail.com>
2024-05-19 19:16:08 -0300

    includ product in folder

---
+++ src/SnackHub.Domain/Contracts/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SnackHub.Domain.Entities;
+
+namespace SnackHub.Domain.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetByIdAsync(Guid id);
+        Task<IEnumerable<Product>> GetByCategoriaAsync(Category category);
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task RemoveAsync(Guid id);
+    }
+}
--- src/SnackHub.Domain/Entities/Product.cs
+++ src/SnackHub.Domain/Entities/Product.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace SnackHub.Domain.Entities
+﻿namespace SnackHub.Domain.Entities
 {
     public class Product
     {
