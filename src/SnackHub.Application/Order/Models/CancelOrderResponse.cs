cd4d541682e9ad5986f89d9b5ca51fba2ae7055d
Diego Bim <1907768+diegobim@users.noreply.github.com>
2024-05-19 19:51:06 -0300

    feat: add CancelOrder use case

---
+++ src/SnackHub.Application/Order/Models/CancelOrderResponse.cs
@@ -0,0 +1,5 @@
+using Flunt.Notifications;
+
+namespace SnackHub.Application.Order.Models;
+
+public class CancelOrderResponse : Notifiable<Notification>;
---
+++ src/SnackHub.Application/Order/UseCases/CancelOrderUseCase.cs
@@ -0,0 +1,41 @@
+using SnackHub.Application.Order.Contracts;
+using SnackHub.Application.Order.Models;
+using SnackHub.Domain.Base;
+using SnackHub.Domain.Contracts;
+
+namespace SnackHub.Application.Order.UseCases;
+
+public class CancelOrderUseCase : ICancelOrderUseCase 
+{
+    private readonly IOrderRepository _orderRepository;
+    
+    public CancelOrderUseCase(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public async Task<CancelOrderResponse> Execute(CancelOrderRequest request)
+    {
+        var response = new CancelOrderResponse();
+        
+        var order = await _orderRepository.GetByIdAsync(request.OrderId);
+        if (order is null)
+        {
+            response.AddNotification(nameof(request.OrderId), "Order not found");
+            return response;
+        }
+
+        try
+        {
+            order.Cancel();
+            
+            await _orderRepository.SaveAsync(order);
+        }
+        catch (DomainException e)
+        {
+            response.AddNotification(nameof(request.OrderId), e.Message);
+        }
+        
+        return response;
+    }
+}
