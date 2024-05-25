077f144cbf0d4f2e03b42ee4747d5e15aa3fdc00
Diego Bim <1907768+diegobim@users.noreply.github.com>
2024-05-25 16:10:59 -0300

    chore: add missing TODO comment

--- src/SnackHub.Application/Order/Models/CheckoutOrderRequest.cs
+++ src/SnackHub.Application/Order/Models/CheckoutOrderRequest.cs
@@ -5,5 +5,7 @@ namespace SnackHub.Application.Order.Models;
 public class CheckoutOrderRequest
 {
     public required Guid OrderId { get; init; }
+    
+    // TODO: make this public an expose it on swagger as OneOf, listing all subclasses  
     internal PaymentMethod PaymentMethod { get; init; } = new OnTheHouse();
 }
