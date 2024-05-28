0443a3b0c9735c9b1390c3257c1003127254a676
Diego Bim <1907768+diegobim@users.noreply.github.com>
2024-05-27 21:44:44 -0300

    feat: confirm order by client ID or CPF

--- src/SnackHub.Application/Order/Models/ConfirmOrderRequest.cs
+++ src/SnackHub.Application/Order/Models/ConfirmOrderRequest.cs
@@ -2,7 +2,7 @@ namespace SnackHub.Application.Order.Models;
 
 public class ConfirmOrderRequest
 {
-    public required Guid ClientId { get; init; }
+    public required string Identifier { get; init; }
     public required IEnumerable<Item> Items { get; init; } = [];
 
     public record Item(Guid ProductId, int Quantity);
