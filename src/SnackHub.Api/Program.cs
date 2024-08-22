dc74cdabc17e6bee5e50a777b8898ca77ab3ef16
leonardo.oliveira <eng_leonardokale@outlook.com>
2024-08-21 23:36:49 -0300

    config keycloak

--- src/SnackHub.Api/Program.cs
+++ src/SnackHub.Api/Program.cs
@@ -1,16 +1,88 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SnackHub.Application.Payment.Models;
 using SnackHub.Extensions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://keycloak.forinnovation.com.br/auth/realms/master";
+        options.Audience = "snakhubapi";
+        options.RequireHttpsMetadata = false; // Defina como true em produção
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+        };
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin() // Permite qualquer origem
+                .AllowAnyMethod() // Permite qualquer método HTTP (GET, POST, etc.)
+                .AllowAnyHeader(); // Permite qualquer cabeçalho
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder
     .Services
     .AddEndpointsApiExplorer()
-    .AddSwaggerGen();
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SnackHub API", Version = "v1" });
+
+        c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {
+                    AuthorizationUrl = new Uri("https://keycloak.forinnovation.com.br/realms/snackhub/protocol/openid-connect/auth"),
+                    TokenUrl = new Uri("https://keycloak.forinnovation.com.br/realms/snackhub/protocol/openid-connect/token"),
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { "openid", "OpenID Connect" },
+                        { "profile", "User profile" },
+                        { "email", "User email" }
+                    }
+                }
+            }
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "oauth2"
+                    }
+                },
+                new[] { "openid", "profile", "email" }
+            }
+        });
+    });
 
 builder
     .Services
@@ -22,14 +94,22 @@ builder
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+ if (app.Environment.IsDevelopment())
+ {
     app.UseSwagger();
-    app.UseSwaggerUI();
-// }
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Snakhub API v1");
 
-app.UseHttpsRedirection();
+        c.OAuthClientId("snakhubapi");
+        c.OAuthClientSecret("Xl38f3J22o6ikk4LnoW8Wyykn1QShZYJ");
+        c.OAuthUsePkce();
+        c.OAuthAppName("Snackhub - Swagger");
+    });
+}
+
+//app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseMongoDbConventions();
 app.MapControllers();
