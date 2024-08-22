a1fa86ecaa6da329494a4ed03a897e2ef37a69ab
leonardo.oliveira <eng_leonardokale@outlook.com>
2024-08-22 00:11:05 -0300

    Controller para ver o usuario logado

---
+++ src/SnackHub.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SnackHub.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/v1")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("me")]
+        [Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var userName = User.Identity?.Name;
+
+            if (userName == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+
+            return Ok(new
+            {
+                UserName = userName,
+                Claims = claims
+            });
+        }
+    }
+}
