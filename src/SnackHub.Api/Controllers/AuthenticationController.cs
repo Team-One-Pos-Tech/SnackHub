3ba017a7b252ce07f80d236234017ec40371b533
Kássio San <kciosantana@gmail.com>
2024-08-30 00:24:50 -0300

    Register client on sign up

--- src/SnackHub.Api/Controllers/AuthenticationController.cs
+++ src/SnackHub.Api/Controllers/AuthenticationController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SnackHub.Application.Client.Contracts;
 using SnackHub.Application.Client.Models;
 using SnackHub.Domain.Contracts;
 using SnackHub.Domain.Models.Gateways;
+using SnackHub.Extensions;
 using SnackHub.Models;
 
 namespace SnackHub.Controllers;
@@ -10,32 +12,44 @@ namespace SnackHub.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthenticationController(IConfiguration Configuration, ISignUpFunctionGateway signUpFunctionGateway, ISignInFunctionGateway signInFunctionGateway) : ControllerBase
+public class AuthenticationController(IConfiguration Configuration, 
+    ISignUpFunctionGateway signUpFunctionGateway, 
+    ISignInFunctionGateway signInFunctionGateway, 
+    IRegisterClientUseCase registerClientUseCase) : ControllerBase
 {
     [HttpPost, Route("signup")]
     public async Task<IActionResult> SignUp([FromBody] RegisterClientRequest user)
     {
         var defaultPassword = Environment.GetEnvironmentVariable("DEFAULT_USERS_PASSWORD");
         
-        var signUpRequest = new SignUpRequest()
+        var request = new RegisterClientRequest(user.Name, user.CPF);
+
+        var response = await registerClientUseCase.Execute(request);
+        
+        if (!response.IsValid) 
         {
-            Name = user.Name,
-            Cpf = user.CPF,
-            Password = defaultPassword!
-        };
+            return ValidationProblem(ModelState.AddNotifications(response.Notifications));
+        }
 
         try
         {
+            var signUpRequest = new SignUpRequest()
+            {
+                Name = user.Name,
+                Cpf = user.CPF,
+                Password = defaultPassword!
+            };
+            
             await signUpFunctionGateway.Execute(signUpRequest);
+
+            return Ok();
         }
         catch (Exception e)
         {
             return BadRequest(e.Message);
         }
-
-        return Ok();
     }
-    
+
     [HttpPost, Route("signin")]
     public async Task<IActionResult> SignIn([FromBody] LoginModel user)
     {
@@ -50,7 +64,8 @@ public class AuthenticationController(IConfiguration Configuration, ISignUpFunct
         try
         {
             var authResponse = await signInFunctionGateway.Execute(signInRequest);
-            return Ok(authResponse);
+            
+            return Ok(authResponse.AuthenticationResult.IdToken);
         }
         catch (Exception e)
         {
