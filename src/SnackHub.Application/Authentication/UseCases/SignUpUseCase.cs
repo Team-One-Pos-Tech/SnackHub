1d44d75d2d0defd2ec4d221225b34fab15dd10fd
Kássio San <kciosantana@gmail.com>
2024-09-20 17:18:55 -0300

    RegisterClient (GREEN)

--- src/SnackHub.Application/Authentication/UseCases/SignUpUseCase.cs
+++ src/SnackHub.Application/Authentication/UseCases/SignUpUseCase.cs
@@ -1,4 +1,6 @@
-﻿using SnackHub.Domain.Contracts;
+﻿using SnackHub.Application.Client.Contracts;
+using SnackHub.Application.Client.Models;
+using SnackHub.Domain.Contracts;
 using SnackHub.Domain.Models.Gateways;
 using System;
 using System.Collections.Generic;
@@ -8,12 +10,14 @@ using System.Threading.Tasks;
 
 namespace SnackHub.Application.Authentication.UseCases
 {
-    public class SignUpUseCase(ISignUpFunctionGateway signUpFunctionGateway, Client.Contracts.IRegisterClientUseCase @object)
+    public class SignUpUseCase(ISignUpFunctionGateway signUpFunctionGateway, IRegisterClientUseCase registerClient)
     {
         private readonly ISignUpFunctionGateway _signUpFunctionGateway = signUpFunctionGateway;
 
         public async Task Execute(SignUpRequest request)
         {
+            var response = await registerClient.Execute(new RegisterClientRequest(request.Name, request.Cpf));
+
             await _signUpFunctionGateway.Execute(request);
         }
     }
