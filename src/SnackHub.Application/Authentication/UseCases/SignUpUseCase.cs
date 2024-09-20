ba3655bc1a2f2b9dd4c80940a20ca65138e0e5fe
Kássio San <kciosantana@gmail.com>
2024-09-20 17:03:40 -0300

    RegisterUser (RED)

---
+++ src/SnackHub.Application/Authentication/UseCases/SignUpUseCase.cs
@@ -0,0 +1,25 @@
+﻿using SnackHub.Domain.Contracts;
+using SnackHub.Domain.Models.Gateways;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackHub.Application.Authentication.UseCases
+{
+    public class SignUpUseCase
+    {
+        private ISignUpFunctionGateway @object;
+
+        public SignUpUseCase(ISignUpFunctionGateway @object)
+        {
+            this.@object = @object;
+        }
+
+        public Task Execute(SignUpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/SnackHub.Application.Tests/UseCases/SignUpShould.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using SnackHub.Application.Authentication.Models;
+using SnackHub.Application.Authentication.UseCases;
+using SnackHub.Domain.Contracts;
+using SnackHub.Domain.Models.Gateways;
+using SnackHub.Domain.Models.Gateways.Models;
+
+namespace SnackHub.Application.Tests.UseCases
+{
+    internal class SignUpShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task RegisterUser()
+        {
+            // Arrange
+
+            var mockSignUpFunctionGateway = new Mock<ISignUpFunctionGateway>();
+
+            mockSignUpFunctionGateway
+                .Setup(gateway => gateway.Execute(It.IsAny<SignUpRequest>()));
+
+            var signInUseCase = new SignUpUseCase(mockSignUpFunctionGateway.Object);
+
+            var request = new SignUpRequest("Ednaldo Pereira", "12345678911", "Default", "email@email.com");
+
+            // Act
+
+            await signInUseCase.Execute(request);
+
+            // Assert
+
+            mockSignUpFunctionGateway.Verify(
+                gateway => gateway.Execute(It.Is<SignUpRequest>(req => req.Cpf == request.Cpf)),
+                Times.Once
+            );
+
+        }
+    }
+}
